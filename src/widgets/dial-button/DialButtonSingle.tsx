6e4b517a88357df93e6acc4373a46b065bddc3f1
usiqwerty <benlogin1@protonmail.com>
2024-05-17 11:08:57 +0500

    mobile long press support

--- src/widgets/dial-button/DialButtonSingle.tsx
+++ src/widgets/dial-button/DialButtonSingle.tsx
@@ -1,13 +1,15 @@
 import "./DialButton.css"
-import {Frequency} from "react-frequency";
 
 function DialButtonSingle({value, onPress, onRelease}: { value: string, onPress: any, onRelease: any }) {
 
-
     return <button className={"dial-btn dial-btn-single"}
                    value={value}
-                   onMouseDown={onPress}
-                   onMouseUp={onRelease}>{value}</button>
+                   onTouchStart={onPress}
+                   onTouchEnd={onRelease}
+
+                   onMouseDown={'ontouchend' in window? ()=>{} :onPress}
+                   onMouseUp={'ontouchstart' in window?  ()=>{}: onRelease}
+        >{value}</button>
 }
 
 export default DialButtonSingle;
--- src/widgets/dial-pad/DialPad.tsx
+++ src/widgets/dial-pad/DialPad.tsx
@@ -12,44 +12,46 @@ let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 function DialPad({pad_layout, type}: { type: string, pad_layout: string[][] }) {
     const [isPressed, setIsPressed] = useState(false);
     const [allowStop, setAllowStop] = useState(false);
-    const [oscA, setOscA] = useState({} as OscillatorNode);
-    const [oscB, setOscB] = useState({} as OscillatorNode);
 
-    function play_tone(freq: number, dur: number) {
-        const osc = audioCtx.createOscillator();
+    const [playingOscillators, setPlayingOscillators] = useState([] as OscillatorNode[]);
 
-        osc.type = 'sine';
-        osc.frequency.value = freq;
-        osc.connect(audioCtx.destination);
-        osc.start();
-        setAllowStop(false);
+    function play_tone(freq: number, [dur, count]: [number, number]) {
+        let oscillators = [];
+        for (let i = 0; i < count; i++) {
+            const osc = audioCtx.createOscillator();
 
-        setTimeout(() => setAllowStop(true), dur);
-        return osc;
+            osc.frequency.value = freq;
+            osc.connect(audioCtx.destination);
+            osc.start(audioCtx.currentTime + dur / 1000 * 2 * i);
+            oscillators.push(osc);
+            
+            setTimeout(() => {
+                setAllowStop(true);
+            }, dur * (2 * i + 1))
+        }
+        return oscillators;
     }
 
-    function play_sound([a, b]: [number, number], duration: number) {
-        setOscA(play_tone(a, duration));
-        setOscB(play_tone(b, duration));
-
+    function play_sound([a, b]: [number, number], duration: [number, number]) {
+        let newosc = playingOscillators.concat(play_tone(a, duration));
+        newosc = newosc.concat(play_tone(b, duration));
+        setPlayingOscillators(newosc);
     }
 
     function ccc(i: string) {
         setIsPressed(true);
-        const snd = tone(i);
-        const [dur, count] = duration(i);
-
-        for (let i = 0; i < count; i++) {
-            setTimeout(() => play_sound(snd, dur), dur * (i * 2));
-        }
+        setAllowStop(false);
+        play_sound(tone(i), duration(i));
     }
 
     useEffect(() => {
-        if (!isPressed && allowStop) {
-            oscA.stop();
-            oscB.stop();
+        if (playingOscillators.length && allowStop) {
+            if(!isPressed) {
+                playingOscillators.forEach(osc => osc.stop())
+                setPlayingOscillators([]);
+            }
         }
-    }, [allowStop, isPressed, oscA, oscB]);
+    }, [allowStop, isPressed, playingOscillators]);
 
     return <div id={"dial-pad"} className={type}>
         {pad_layout.map(row => row.map((i) => {
